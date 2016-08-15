d1cbb459b907e1168bbd6584ed00d0b6b0ae438b
vronnieli <vronnieli@gmail.com>
2016-08-15 14:04:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+#Create
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(name: params["name"], content: params["content"])
+    redirect to '/posts'
+  end
+
+#Read
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+#update
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params["name"]
+    @post.content = params["content"]
+    @post.save
+    redirect to "/posts/#{@post.id}"
+  end
+
+#delete
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    # flash[:notice] = "#{@post.name} was deleted"
+    # redirect to '/posts'
+    @posts = Post.all
+    erb :index
+  end
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,12 @@
 #Placeholder for a model
+class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+  #
+  # def initialize(name, content)
+  #
+  #   @name = name
+  #   @content = content
+  # end
+
+end
